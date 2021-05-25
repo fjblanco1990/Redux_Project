67b790cdbebdcc8dc263892e66e218f507904d73
fjblanco1990 <fjblanco1990@gmail.com>
2021-05-25 15:20:19 -0500

    Se avanza en el curso clase 31

--- src/app/contador/contador.reducer.ts
+++ src/app/contador/contador.reducer.ts
@@ -1,15 +1,25 @@
-import { Action } from "@ngrx/store";
+import {  createReducer, on } from "@ngrx/store";
 import { drecrementar, incrementar } from "./contador.actions";
 
+export const initialState = 20;
 
+const _contadorReducer = createReducer(initialState,
+    on(incrementar, (state) => state + 1),
+    on(drecrementar, (state) => state - 1)
+  );
 
-export function contadorReducer(state: number = 10, action: Action) {
-    switch (action.type) { 
-        case incrementar.type:
-            return state + 1;
-        case drecrementar.type:
-            return state - 1;   
-        default:
-            return state; 
-    }
-}
+  export function contadorReducer(state: any, action: any) {
+      return _contadorReducer(state, action);
+  }
+  
+
+// export function contadorReducer(state: number = 10, action: Action) {
+//     switch (action.type) { 
+//         case incrementar.type:
+//             return state + 1;
+//         case drecrementar.type:
+//             return state - 1;   
+//         default:
+//             return state; 
+//     }
+// }
